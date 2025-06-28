fc3cd440acf54ab72d7b322b261e3a1bd5abeb6e
Njambi <njambim869@gmail.com>
2025-06-28 22:58:13 +0300

    Updated api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import toast from 'react-hot-toast'
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: import.meta.env.PROD ? '/api' : 'http://localhost:3000/api',
   timeout: 10000,
 })
 
